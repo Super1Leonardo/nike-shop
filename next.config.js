e3cab253436d895d7ef68ab248fac74014ddd38d
SupLeonardo <levabaluevvv@gmail.com>
2024-10-28 15:30:55 +0500

    hey

--- next.config.js
+++ next.config.js
@@ -1,17 +1,21 @@
 // next.config.mjs
-export default {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'i.imgur.com',
-          pathname: '**',
-        },
-        {
-          protocol: 'https',
-          hostname: 'cdn-icons-png.flaticon.com',
-          pathname: '**',
-        },
-      ],
-    },
-  };
+// export default {
+//     images: {
+//       remotePatterns: [
+//         {
+//           protocol: 'https',
+//           hostname: 'i.imgur.com',
+//           pathname: '**',
+//         },
+//         {
+//           protocol: 'https',
+//           hostname: 'cdn-icons-png.flaticon.com',
+//           pathname: '**',
+//         },
+//       ],
+//     },
+//   module.export
+// };
+module.exports = {
+  Suspense
+}
